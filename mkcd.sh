5af100cb5d520afe3e8e864d5f2443b1df54f6c8
mathias.didier.prestataire <mathias.didier.prestataire@wiztivi.com>
2020-06-09 10:28:00 +0200

    add mkcd

---
+++ mkcd.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# remember to add 'alias mkcd=". mkcd.sh"' into your .*shrc
+
+missing_params(){
+	echo 'missings params'
+	echo 'mkcd "<folder to be created>"'
+}
+
+
+if [ -z "$1" ]
+then
+	missing_params && exit
+fi
+
+mkdir $1 && cd $1
